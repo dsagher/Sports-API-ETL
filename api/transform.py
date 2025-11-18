ea64a714eb1f369367b3de6bce9b2d7d6d91a6f6
Dan Sagher <daniel.sagher1@gmail.com>
2025-11-18 18:53:05 -0500

    update test1

--- api/transform.py
+++ api/transform.py
@@ -15,7 +15,7 @@ def transform_leagues(data):
         seasons = league['seasons'][0]
         coverage = league['seasons'][0]['coverage']
         fixtures = league['season2']
-        # lalalala
+        # lalala
 
 
         transformed_data.append({
